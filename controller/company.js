48e3696e4cf959671007289434a043c0a10f7a40
thayour <temyking@gmail.com>
2024-09-29 11:09:52 +0100

    com

--- controller/company.js
+++ controller/company.js
@@ -47,7 +47,7 @@ const register = async (req, res, next) => {
         res.status(201).json({
             success: true,
             message: "Company Account Create Successfully",
-            account: {
+            user: {
                 ...account._doc,
                 password: undefined,
             },
