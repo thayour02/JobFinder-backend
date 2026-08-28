309d6748ef8b78735f1a35bbf3c8242abbaac267
thayour02 <you@example.com>
2026-08-28 04:31:04 -0300

    db

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ const app = express()
 //MIDDLEWARES
 app.use(cors({
     origin:"https://thayourjobfinderapp.netlify.app", 
-    // origin:'http://localhost:5173',
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
